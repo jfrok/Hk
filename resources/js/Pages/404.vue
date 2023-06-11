9f6366ef74db8d8cb19822b58168aa010a8c2275
jaafar osama <jaafarosama75@gmail.com>
2023-06-11 03:13:09 +0200

    adding fallback page + some changes

---
+++ resources/js/Pages/404.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1 style="display: flex;justify-content: center">404</h1>
+</template>
+<script>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+
+export default {
+    layout:AuthenticatedLayout
+}
+</script>
